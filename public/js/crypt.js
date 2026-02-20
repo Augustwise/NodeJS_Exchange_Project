63cbc6f34a3ac02d1c29f671b0a85b976ffcde5f
easyddr <dmitro050707@gmail.com>
2026-02-20 18:19:26 +0200

    Crypto page small changes (new tab "Users Projects")

--- public/js/crypt.js
+++ public/js/crypt.js
@@ -1,44 +1,45 @@
 function switchMainTab(event, tabId) {
-            document.querySelectorAll('.tab-content').forEach(tab => tab.classList.remove('active'));
-            document.querySelectorAll('.tab-btn').forEach(btn => btn.classList.remove('active'));
-            
-            document.getElementById(tabId).classList.add('active');
-            event.currentTarget.classList.add('active');
-        }
+    document.querySelectorAll('.tab-content').forEach(tab => tab.classList.remove('active'));
+    document.querySelectorAll('.tab-btn').forEach(btn => btn.classList.remove('active'));
+    document.querySelectorAll('.tab-btn-right').forEach(btn => btn.classList.remove('active'));
 
-        function switchChart(event, coinId) {
-            document.querySelectorAll('.tv-chart').forEach(chart => chart.classList.remove('active'));
-            document.querySelectorAll('.coin-btn').forEach(btn => btn.classList.remove('active'));
-            
-            document.getElementById(coinId).classList.add('active');
-            event.currentTarget.classList.add('active');
-        }
+    document.getElementById(tabId).classList.add('active');
+    event.currentTarget.classList.add('active');
+}
 
-        function createChart(container, symbol) {
-            new TradingView.widget({
-                width: "100%",
-                height: 560, 
-                symbol: symbol,
-                interval: "60",
-                timezone: "Europe/Kiev",
-                theme: "dark",
-                style: "1",
-                locale: "uk",
-                allow_symbol_change: false,
-                container_id: container,
-                backgroundColor: "rgba(22, 27, 34, 0)", 
-                gridColor: "rgba(255, 255, 255, 0.05)"
-            });
-        }
+function switchChart(event, coinId) {
+    document.querySelectorAll('.tv-chart').forEach(chart => chart.classList.remove('active'));
+    document.querySelectorAll('.coin-btn').forEach(btn => btn.classList.remove('active'));
 
-        document.addEventListener("DOMContentLoaded", () => {
-            createChart("btc", "BINANCE:BTCUSDT");
-            createChart("eth", "BINANCE:ETHUSDT");
-            createChart("sol", "BINANCE:SOLUSDT");
-            createChart("ada", "BINANCE:ADAUSDT");
-            createChart("bnb", "BINANCE:BNBUSDT");
-            createChart("xpr", "BINANCE:XRPUSDT");
-            createChart("ton", "BINANCE:TONUSDT");
-            createChart("doge", "BINANCE:DOGEUSDT");
-            createChart("pepe", "BINANCE:PEPEUSDT");
-        });
+    document.getElementById(coinId).classList.add('active');
+    event.currentTarget.classList.add('active');
+}
+
+function createChart(container, symbol) {
+    new TradingView.widget({
+        width: "100%",
+        height: 560,
+        symbol: symbol,
+        interval: "60",
+        timezone: "Europe/Kiev",
+        theme: "dark",
+        style: "1",
+        locale: "uk",
+        allow_symbol_change: false,
+        container_id: container,
+        backgroundColor: "rgba(22, 27, 34, 0)",
+        gridColor: "rgba(255, 255, 255, 0.05)"
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    createChart("btc", "BINANCE:BTCUSDT");
+    createChart("eth", "BINANCE:ETHUSDT");
+    createChart("sol", "BINANCE:SOLUSDT");
+    createChart("ada", "BINANCE:ADAUSDT");
+    createChart("bnb", "BINANCE:BNBUSDT");
+    createChart("xpr", "BINANCE:XRPUSDT");
+    createChart("ton", "BINANCE:TONUSDT");
+    createChart("doge", "BINANCE:DOGEUSDT");
+    createChart("pepe", "BINANCE:PEPEUSDT");
+});
